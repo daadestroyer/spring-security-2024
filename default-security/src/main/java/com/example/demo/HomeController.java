e7199b0f47075fd990168d3c88c565ea0270bae4
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-08-17 13:56:54 +0530

    code added...

--- default-security/src/main/java/com/example/demo/HomeController.java
+++ default-security/src/main/java/com/example/demo/HomeController.java
@@ -1,19 +1,14 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/confidential")
 public class HomeController {
-
-    @Value("${spring.security.user.name}")
-    private String username;
-    @GetMapping("/dashboard")
-    public String dashboard(){
-        return "Welcome back "+username;
+    @GetMapping
+    public String home(){
+        return "You have accessed confidential data";
     }
-
 }
